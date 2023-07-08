11546d6801ab1aefbe5119231d0507fdd6ead5ea
Fidel Yiu <fidel-yiu@foxmail.com>
2023-07-08 18:17:25 +0800

    change doc

---
+++ yiu-tree-js/assets/type_TreeNodeInfo_index.md.fe7e28d4.js
@@ -0,0 +1,35 @@
+import{_ as s,c as n,o as a,a as l}from"./app.9a4a6452.js";const F=JSON.parse('{"title":"Tree节点信息类型","description":"","frontmatter":{"title":"Tree节点信息类型"},"headers":[],"relativePath":"type/TreeNodeInfo/index.md","lastUpdated":1679297741000}'),p={name:"type/TreeNodeInfo/index.md"},o=l(`<h1 id="tree节点信息类型" tabindex="-1">Tree节点信息类型 <a class="header-anchor" href="#tree节点信息类型" aria-hidden="true">#</a></h1><div class="language-typescript"><button title="Copy Code" class="copy"></button><span class="lang">typescript</span><pre class="shiki material-theme-palenight"><code><span class="line"><span style="color:#C792EA;">type</span><span style="color:#A6ACCD;"> </span><span style="color:#FFCB6B;">TreeNodeInfo</span><span style="color:#89DDFF;">&lt;</span><span style="color:#FFCB6B;">T</span><span style="color:#A6ACCD;"> </span><span style="color:#89DDFF;">=</span><span style="color:#A6ACCD;"> </span><span style="color:#FFCB6B;">any</span><span style="color:#89DDFF;">&gt;</span><span style="color:#A6ACCD;"> </span><span style="color:#89DDFF;">=</span><span style="color:#A6ACCD;"> </span><span style="color:#89DDFF;">{</span></span>
+<span class="line"><span style="color:#A6ACCD;">    </span><span style="color:#676E95;font-style:italic;">/**</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">     * 层级数，从1开始</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">     */</span></span>
+<span class="line"><span style="color:#A6ACCD;">    </span><span style="color:#F07178;">level</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#FFCB6B;">number</span></span>
+<span class="line"><span style="color:#A6ACCD;">    </span><span style="color:#676E95;font-style:italic;">/**</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">     * 当前List中索引数，从0开始</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">     */</span></span>
+<span class="line"><span style="color:#A6ACCD;">    </span><span style="color:#F07178;">index</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#FFCB6B;">number</span></span>
+<span class="line"><span style="color:#A6ACCD;">    </span><span style="color:#676E95;font-style:italic;">/**</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">     * 是否是叶子节点</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">     */</span></span>
+<span class="line"><span style="color:#A6ACCD;">    </span><span style="color:#F07178;">isLeaf</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#FFCB6B;">boolean</span></span>
+<span class="line"><span style="color:#A6ACCD;">    </span><span style="color:#676E95;font-style:italic;">/**</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">     * 是否是第一个节点</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">     */</span></span>
+<span class="line"><span style="color:#A6ACCD;">    </span><span style="color:#F07178;">isFirst</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#FFCB6B;">boolean</span></span>
+<span class="line"><span style="color:#A6ACCD;">    </span><span style="color:#676E95;font-style:italic;">/**</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">     * 是否是最后一个节点</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">     */</span></span>
+<span class="line"><span style="color:#A6ACCD;">    </span><span style="color:#F07178;">isLast</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#FFCB6B;">boolean</span></span>
+<span class="line"><span style="color:#A6ACCD;">    </span><span style="color:#676E95;font-style:italic;">/**</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">     * 父节点</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">     */</span></span>
+<span class="line"><span style="color:#A6ACCD;">    </span><span style="color:#F07178;">parent</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#FFCB6B;">T</span><span style="color:#A6ACCD;"> </span><span style="color:#89DDFF;">|</span><span style="color:#A6ACCD;"> </span><span style="color:#FFCB6B;">undefined</span></span>
+<span class="line"><span style="color:#A6ACCD;">    </span><span style="color:#676E95;font-style:italic;">/**</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">     * 节点路径</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">     */</span></span>
+<span class="line"><span style="color:#A6ACCD;">    </span><span style="color:#F07178;">path</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#C792EA;">readonly</span><span style="color:#A6ACCD;"> </span><span style="color:#FFCB6B;">T</span><span style="color:#A6ACCD;">[]</span></span>
+<span class="line"><span style="color:#A6ACCD;">    </span><span style="color:#676E95;font-style:italic;">/**</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">     * 父节点路径</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">     */</span></span>
+<span class="line"><span style="color:#A6ACCD;">    </span><span style="color:#F07178;">parentPath</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#C792EA;">readonly</span><span style="color:#A6ACCD;"> </span><span style="color:#FFCB6B;">T</span><span style="color:#A6ACCD;">[]</span></span>
+<span class="line"><span style="color:#89DDFF;">}</span></span>
+<span class="line"></span></code></pre></div>`,2),e=[o];function t(c,r,y,i,C,A){return a(),n("div",null,e)}const f=s(p,[["render",t]]);export{F as __pageData,f as default};
